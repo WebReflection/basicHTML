8a7ff866c99ece3b13f50f0ec8dda638c296b5f2
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-06-13 14:16:17 +0100

    exporting Element too

--- basichtml.js
+++ basichtml.js
@@ -2,6 +2,7 @@ module.exports = {
   CustomElementRegistry: require('./src/CustomElementRegistry'),
   CustomEvent: require('./src/CustomEvent'),
   Document: require('./src/Document'),
+  Element: require('./src/Element'),
   Event: require('./src/Event'),
   HTMLElement: require('./src/HTMLElement')
 };
