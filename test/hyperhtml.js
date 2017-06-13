30d98d0f83ceb52b165bf61de50be1d07acb0111
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-06-13 14:51:41 +0100

    added hyperHTML example \o/

---
+++ test/hyperhtml.js
@@ -0,0 +1,20 @@
+const {Document} = require('../basichtml');
+const document = new Document();
+
+// hyperHTML needs at least a global document
+// to perform an initial feature detection
+global.document = document;
+const hyperHTML = require('hyperhtml');
+
+// most basic hyperHTML examples in node
+function tick(render) {
+  console.log(render`
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
+    </div>
+  `.innerHTML);
+}
+
+// start ticking
+setInterval(tick, 1000, hyperHTML.bind(document.body));
